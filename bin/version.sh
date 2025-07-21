1ac5e58a444be946cc758f5867310b432505f8d2
xiaochun <90xchun@gmail.com>
2025-07-21 10:29:37 +0800

    No release branch

--- bin/version.sh
+++ bin/version.sh
@@ -4,6 +4,7 @@ releaseVersion=${baseVersion}.${1}
 nextVersion=${baseVersion}.$((${1}+1))-SNAPSHOT
 tagName="v${releaseVersion}"
 ./mvnw versions:set -DnewVersion=${releaseVersion}
+./mvnw clean install
 git add -A
 git commit -m '[shell-release]release version '${releaseVersion}
 git tag ${tagName}
