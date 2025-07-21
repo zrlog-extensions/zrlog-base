e973caf31f36deba2d9d0f03a14cff627a97f053
xiaochun <90xchun@gmail.com>
2025-07-21 11:12:53 +0800

    [shell-release]release version 3.2.8

---
+++ zrlog-service/src/main/java/com/zrlog/business/util/ServiceNativeImageUtils.java
@@ -0,0 +1,31 @@
+package com.zrlog.business.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ServiceNativeImageUtils {
+
+    public static void regResource() throws IOException {
+        List<String> resourceFiles = new ArrayList<>();
+        for (int i = 1; i <= 100; i++) {
+            String filePath = "/conf/update-sql/" + i + ".sql";
+            InputStream inputStream = ServiceNativeImageUtils.class.getResourceAsStream(filePath);
+            if (Objects.nonNull(inputStream)) {
+                resourceFiles.add(filePath);
+            }
+        }
+        resourceFiles.add("/i18n/backend_en_US.properties");
+        resourceFiles.add("/i18n/backend_zh_CN.properties");
+        resourceFiles.add("/zrlog.properties");
+        resourceFiles.add("/conf/website-key-public.json");
+
+        for (String resourceFile : resourceFiles) {
+            try (InputStream in = ServiceNativeImageUtils.class.getResourceAsStream(resourceFile)) {
+                //ignore
+            }
+        }
+    }
+}
