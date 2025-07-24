9dd14537c1d11c2b8f4d39c31abdf8bf5d5ef749
xiaochun <90xchun@gmail.com>
2025-07-24 16:30:16 +0800

    [shell-release]release version 3.2.20

--- zrlog-data/src/main/java/com/zrlog/data/dto/TypeDTO.java
+++ zrlog-data/src/main/java/com/zrlog/data/dto/TypeDTO.java
@@ -1,10 +1,22 @@
 package com.zrlog.data.dto;
 
 public class TypeDTO {
+
+    private String alias;
     private String typeName;
     private String remark;
-    private String alias;
     private Long id;
+    private Long amount;
+    private Long typeamount;
+    private String url;
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
 
     public String getTypeName() {
         return typeName;
@@ -22,19 +34,35 @@ public class TypeDTO {
         this.remark = remark;
     }
 
-    public String getAlias() {
-        return alias;
-    }
-
-    public void setAlias(String alias) {
-        this.alias = alias;
-    }
-
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public Long getTypeamount() {
+        return typeamount;
+    }
+
+    public void setTypeamount(Long typeamount) {
+        this.typeamount = typeamount;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
 }
