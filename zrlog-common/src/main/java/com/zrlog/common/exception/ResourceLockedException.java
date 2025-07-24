97f06e981341416d6a97e19717b5b434ab2499e1
xiaochun <90xchun@gmail.com>
2025-07-24 18:22:35 +0800

    [shell-release]release version 3.2.22

--- zrlog-common/src/main/java/com/zrlog/common/exception/ResourceLockedException.java
+++ zrlog-common/src/main/java/com/zrlog/common/exception/ResourceLockedException.java
@@ -1,9 +1,16 @@
 package com.zrlog.common.exception;
 
+import com.zrlog.util.I18nUtil;
+
 public class ResourceLockedException extends AbstractBusinessException {
 
     @Override
     public int getError() {
         return 9100;
+    }
+
+    @Override
+    public String getMessage() {
+        return I18nUtil.getBackendStringFromRes("resourceBusy");
     }
 }
