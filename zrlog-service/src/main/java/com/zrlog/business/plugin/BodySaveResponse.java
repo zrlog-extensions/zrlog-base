94f38eb56436d1c547c750118e131854ae4d6ac8
xiaochun <90xchun@gmail.com>
2025-07-31 09:43:12 +0800

    [shell-release]release version 3.2.58

--- zrlog-service/src/main/java/com/zrlog/business/plugin/BodySaveResponse.java
+++ zrlog-service/src/main/java/com/zrlog/business/plugin/BodySaveResponse.java
@@ -15,6 +15,7 @@ public class BodySaveResponse extends SimpleHttpResponse implements AutoCloseabl
     private OutputStream outputStream;
     private final File cacheFile;
     private final Lock initLock = new ReentrantLock();
+    private int statusCode;
 
     public BodySaveResponse(HttpRequest request, ResponseConfig responseConfig) {
         super(request, responseConfig);
@@ -48,6 +49,14 @@ public class BodySaveResponse extends SimpleHttpResponse implements AutoCloseabl
         return false;
     }
 
+
+    @Override
+    protected byte[] wrapperBaseResponseHeader(int statusCode) {
+        this.statusCode = statusCode;
+        return super.wrapperBaseResponseHeader(statusCode);
+    }
+
+
     @Override
     protected void send(byte[] bytes, boolean body, boolean close) {
         if (Objects.isNull(cacheFile)) {
@@ -79,6 +88,9 @@ public class BodySaveResponse extends SimpleHttpResponse implements AutoCloseabl
     }
 
     public File getCacheFile() {
+        if (statusCode != 200) {
+            return null;
+        }
         return cacheFile;
     }
 
