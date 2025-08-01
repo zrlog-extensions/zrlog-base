25749fd32df0d4e6f4dd699715aaf3b9c13148da
xiaochun <90xchun@gmail.com>
2025-08-01 10:18:04 +0800

    [shell-release]release version 3.2.61

---
+++ zrlog-service/src/main/java/com/zrlog/business/util/ArticleHelpers.java
@@ -0,0 +1,39 @@
+package com.zrlog.business.util;
+
+import com.hibegin.common.dao.dto.PageData;
+import com.hibegin.common.util.StringUtils;
+import com.zrlog.data.dto.ArticleBasicDTO;
+import com.zrlog.util.ParseUtil;
+
+import java.util.List;
+
+/**
+ * 与文章相关的业务代码
+ */
+public class ArticleHelpers {
+
+    /**
+     * 高亮用户检索的关键字
+     */
+    public static void wrapperSearchKeyword(PageData<ArticleBasicDTO> data, String keywords) {
+        if (StringUtils.isEmpty(keywords)) {
+            return;
+        }
+        List<ArticleBasicDTO> logs = data.getRows();
+        if (logs == null || logs.isEmpty()) {
+            return;
+        }
+        for (ArticleBasicDTO log : logs) {
+            String title = log.getTitle();
+            String content = log.getContent();
+            String digest = log.getDigest();
+            log.setTitle(ParseUtil.wrapperKeyword(title, keywords));
+            String tryWrapperDigest = ParseUtil.wrapperKeyword(digest, keywords);
+            if (tryWrapperDigest != null && tryWrapperDigest.length() != digest.length()) {
+                log.setDigest(tryWrapperDigest);
+            } else {
+                log.setDigest(ParseUtil.wrapperKeyword(ParseUtil.removeHtmlElement(content), keywords));
+            }
+        }
+    }
+}
