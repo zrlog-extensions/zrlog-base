f1b876834807ed3dc3fed7ef7e94a92162525d15
xiaochun <90xchun@gmail.com>
2025-12-02 22:31:48 +0800

    add

--- zrlog-common/src/main/java/com/hibegin/common/util/http/handle/HttpResponseJsonHandle.java
+++ zrlog-common/src/main/java/com/hibegin/common/util/http/handle/HttpResponseJsonHandle.java
@@ -3,9 +3,12 @@ package com.hibegin.common.util.http.handle;
 import com.google.gson.Gson;
 import com.hibegin.common.util.IOUtil;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Optional;
+import java.util.zip.GZIPInputStream;
 
 public class HttpResponseJsonHandle<T> extends HttpHandle<T> {
 
@@ -17,7 +20,16 @@ public class HttpResponseJsonHandle<T> extends HttpHandle<T> {
 
     @Override
     public boolean handle(HttpRequest request, HttpResponse<InputStream> response) {
-        String jsonStr = IOUtil.getStringInputStream(response.body());
+        Optional<String> s = response.headers().firstValue("Content-Encoding");
+        InputStream inputStream = response.body();
+        if (s.isPresent() && s.get().equals("gzip")) {
+            try {
+                inputStream = new GZIPInputStream(inputStream);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        String jsonStr = IOUtil.getStringInputStream(inputStream);
         if (response.statusCode() == 200) {
             setT(new Gson().fromJson(jsonStr, clazz));
         } else {
